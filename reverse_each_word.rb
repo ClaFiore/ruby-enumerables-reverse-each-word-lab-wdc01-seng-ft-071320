cdc68441d2ad9d008f975d223c3cb9d41f8034ed
Claudia Borghini <claborghini@gmail.com>
2020-07-06 15:00:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
 def reverse_each_word(str)
-  reverse_array = %w[str].collect do |word| word.reverse end
+  reverse_array = str.split.collect do |word| word.reverse end
   reverse_array.join(" ")
 end
